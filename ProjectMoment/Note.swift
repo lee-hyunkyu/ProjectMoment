5961692c85b66e6d19c053055c7c06cd08e97c01
Hyunkyu Lee <hkl0902@gmail.com>
2016-08-05 14:47:33 -0700

    Creates a dictionary of default flag values for easy reference

--- ProjectMoment/Note.swift
+++ ProjectMoment/Note.swift
@@ -27,12 +27,35 @@ class Note: NSManagedObject {
     
     // Default Flag Color as (Red, Green, Blue, Alpha)
     struct DefaultFlagColor {
-        static let Low = (0, 0, 0, 0)
-        static let Medium_Low = (0, 0, 0, 0)
-        static let Medium = (0, 0, 0, 0)
-        static let Medium_High = (0, 0, 0, 0)
-        static let High = (0, 0, 0, 0)
+        static let Low: (Float, Float, Float, Float) = (0.0, 0.0, 0.0, 0.0)
+        static let Medium_Low: (Float, Float, Float, Float) = (0.0, 0.0, 0.0, 0.0)
+        static let Medium: (Float, Float, Float, Float) = (0.0, 0.0, 0.0, 0.0)
+        static let Medium_High: (Float, Float, Float, Float) = (0.0, 0.0, 0.0, 0.0)
+        static let High: (Float, Float, Float, Float) = (0.0, 0.0, 0.0, 0.0)
     }
+    
+    private lazy var defaultFlags: [Int: Flag] = {
+        var defaultFlags = [Int:Flag]()
+        
+        // Set Low
+        var (red, green, blue, alpha) = DefaultFlagColor.Low
+        defaultFlags[0] = Flag.Low(0, UIColor(colorLiteralRed: red, green: green, blue: blue, alpha: alpha))
+        // Set Medium_Low
+        (red, green, blue, alpha) = DefaultFlagColor.Medium_Low
+        defaultFlags[1] = Flag.Low(1, UIColor(colorLiteralRed: red, green: green, blue: blue, alpha: alpha))
+        // Set Medium
+        (red, green, blue, alpha) = DefaultFlagColor.Medium
+        defaultFlags[2] = Flag.Low(2, UIColor(colorLiteralRed: red, green: green, blue: blue, alpha: alpha))
+        // Set Medium_High
+        (red, green, blue, alpha) = DefaultFlagColor.Medium_High
+        defaultFlags[3] = Flag.Low(3, UIColor(colorLiteralRed: red, green: green, blue: blue, alpha: alpha))
+        // Set High
+        (red, green, blue, alpha) = DefaultFlagColor.High
+        defaultFlags[4] = Flag.Low(4, UIColor(colorLiteralRed: red, green: green, blue: blue, alpha: alpha))
+        return defaultFlags
+    }()
+    
+    
     
     
     
