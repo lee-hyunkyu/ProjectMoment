e8fb23ea9eeb9f0767dff29e34387dd725515953
Hyunkyu Lee <hkl0902@gmail.com>
2016-08-05 16:25:43 -0700

    Creates files for VC showing the notebooks not currently archived

---
+++ ProjectMoment/NotebookTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NotebookTableViewCell.swift
+//  ProjectMoment
+//
+//  Created by Hyunkyu Lee on 8/5/16.
+//  Copyright © 2016 Hyunkyu Lee. All rights reserved.
+//
+
+import UIKit
+
+class NotebookTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
