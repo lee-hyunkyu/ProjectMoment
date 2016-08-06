5f42df4a442355b3fe221360e13f98e0b3989ca1
Hyunkyu Lee <hkl0902@gmail.com>
2016-08-05 17:00:41 -0700

    Creates a very basic UITableViewController
    The VC does not actually inherit from UITableViewController
    Instead, it is a UIViewController and everything is hardcoded in
    As this is something I was not sure I could do, I use random test cells.
    Custom cells will be made in the future

--- ProjectMoment/NotebookTableViewController.swift
+++ ProjectMoment/NotebookTableViewController.swift
@@ -8,16 +8,45 @@
 
 import UIKit
 
-class NotebookTableViewController: UIViewController {
+class NotebookTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
+    struct Cells {
+        static let Notebook = "Notebook Cell Identifier"
+    }
+    
+    var tableView: UITableView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView = UITableView(frame: UIScreen.mainScreen().applicationFrame, style: .Plain)
+        tableView?.autoresizingMask = UIViewAutoresizing.FlexibleHeight
+        tableView?.delegate = self
+        tableView?.dataSource = self
+        self.view = tableView
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier(Cells.Notebook)
+        if cell == nil {
+            cell = UITableViewCell(style: .Default, reuseIdentifier: Cells.Notebook)
+        }
+        cell?.textLabel?.text = "\(indexPath.row)"
+        return cell!
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
 
 
