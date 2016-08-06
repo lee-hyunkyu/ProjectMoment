2e808cf8fde782b832dfdbf8114ffe29656bbbe5
Hyunkyu Lee <hkl0902@gmail.com>
2016-08-05 17:14:34 -0700

    Sets the table view controller to be in side a UINavCon

--- ProjectMoment/AppDelegate.swift
+++ ProjectMoment/AppDelegate.swift
@@ -16,7 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        let tableVC = window?.rootViewController
+        let navCon = NavigationViewController(rootViewController: tableVC!)
+        window?.rootViewController = navCon
+        
         return true
     }
 
