70721ec2e600b54bf1a1dc79e6b0fe0d5727d4c0
Hyunkyu Lee <hkl0902@gmail.com>
2016-08-07 14:36:10 -0700

    Replaces the emppty model files with the files from the Create_Model branch

--- ProjectMoment/Location.swift
+++ ProjectMoment/Location.swift
@@ -2,7 +2,7 @@
 //  Location.swift
 //  ProjectMoment
 //
-//  Created by Hyunkyu Lee on 8/7/16.
+//  Created by Hyunkyu Lee on 8/5/16.
 //  Copyright © 2016 Hyunkyu Lee. All rights reserved.
 //
 
@@ -12,6 +12,21 @@ import CoreData
 
 class Location: NSManagedObject {
 
-// Insert code here to add functionality to your managed object subclass
+    struct Names {
+        static let EntityName = "Location"
+    }
+    
+    class func createLocationWith(name: String?, longitude: Double?, latitude: Double?, address: String?, inManagedObjectContext managedObjectContext: NSManagedObjectContext?) -> Location? {
+        guard let context = managedObjectContext else { return nil }
+        if let location = NSEntityDescription.insertNewObjectForEntityForName(Names.EntityName, inManagedObjectContext: context) as? Location {
+            location.id = NSUUID().UUIDString
+            location.address = address
+            location.latitude = "\(latitude)"
+            location.longitude = "\(longitude)"
+            location.name = name
+            return location
+        }
+        return nil
+    }
 
 }
--- ProjectMoment/Note.swift
+++ ProjectMoment/Note.swift
@@ -2,16 +2,102 @@
 //  Note.swift
 //  ProjectMoment
 //
-//  Created by Hyunkyu Lee on 8/7/16.
+//  Created by Hyunkyu Lee on 8/5/16.
 //  Copyright © 2016 Hyunkyu Lee. All rights reserved.
 //
 
 import Foundation
 import CoreData
+import UIKit
 
 
 class Note: NSManagedObject {
-
-// Insert code here to add functionality to your managed object subclass
-
+    
+    struct Names {
+        static let EntityName = "Note"
+    }
+    
+    enum Flag {
+        case Low(Int, UIColor)
+        case Medium_Low(Int, UIColor)
+        case Medium(Int, UIColor)
+        case Medium_High(Int, UIColor)
+        case High(Int, UIColor)
+    }
+    
+    // Default Flag Color as (Red, Green, Blue, Alpha)
+    struct DefaultFlagColor {
+        static let Low: (Float, Float, Float, Float) = (0.0, 0.0, 0.0, 0.0)
+        static let Medium_Low: (Float, Float, Float, Float) = (0.0, 0.0, 0.0, 0.0)
+        static let Medium: (Float, Float, Float, Float) = (0.0, 0.0, 0.0, 0.0)
+        static let Medium_High: (Float, Float, Float, Float) = (0.0, 0.0, 0.0, 0.0)
+        static let High: (Float, Float, Float, Float) = (0.0, 0.0, 0.0, 0.0)
+    }
+    
+    static private let defaultFlags: [Int: Flag] = {
+        var defaultFlags = [Int:Flag]()
+        
+        // Set Low
+        var (red, green, blue, alpha) = DefaultFlagColor.Low
+        defaultFlags[0] = Flag.Low(0, UIColor(colorLiteralRed: red, green: green, blue: blue, alpha: alpha))
+        // Set Medium_Low
+        (red, green, blue, alpha) = DefaultFlagColor.Medium_Low
+        defaultFlags[1] = Flag.Low(1, UIColor(colorLiteralRed: red, green: green, blue: blue, alpha: alpha))
+        // Set Medium
+        (red, green, blue, alpha) = DefaultFlagColor.Medium
+        defaultFlags[2] = Flag.Low(2, UIColor(colorLiteralRed: red, green: green, blue: blue, alpha: alpha))
+        // Set Medium_High
+        (red, green, blue, alpha) = DefaultFlagColor.Medium_High
+        defaultFlags[3] = Flag.Low(3, UIColor(colorLiteralRed: red, green: green, blue: blue, alpha: alpha))
+        // Set High
+        (red, green, blue, alpha) = DefaultFlagColor.High
+        defaultFlags[4] = Flag.Low(4, UIColor(colorLiteralRed: red, green: green, blue: blue, alpha: alpha))
+        return defaultFlags
+    }()
+    
+    // Location will be current location
+    class func createNoteWith(title: String?, note: String?, flag: Int?, inManagedObjectContext managedObjectContext: NSManagedObjectContext?) -> Note? {
+        guard let context = managedObjectContext else { return nil }
+        if let note = NSEntityDescription.insertNewObjectForEntityForName(Names.EntityName, inManagedObjectContext: context) as? Note {
+            note.id = NSUUID().UUIDString                                       // Create a unique ID
+            note.titleText = title                                              // Sets title text regardless of whether it can be unwrapped or not
+            if let flagValue = flag {
+                if flagValue > defaultFlags.count || flagValue < 0 {
+                    note.flag = 0
+                }
+            }else {
+                note.flag = flag
+            }
+            note.time = NSDate()
+            // Todo - Add Location Ability
+            return note
+        }
+        
+        return nil
+    }
+    
+    // Location is given
+    class func createNoteWith(title: String?, note: String?, flag: Int?, location: Location?, inManagedObjectContext managedObjectContext: NSManagedObjectContext?) -> Note? {
+        guard let context = managedObjectContext else { return nil }
+        if let note = NSEntityDescription.insertNewObjectForEntityForName(Names.EntityName, inManagedObjectContext: context) as? Note {
+            note.id = NSUUID().UUIDString                                       // Create a unique ID
+            note.titleText = title                                              // Sets title text regardless of whether it can be unwrapped or not
+            if let flagValue = flag {
+                if flagValue > defaultFlags.count || flagValue < 0 {
+                    note.flag = 0
+                }
+            }else {
+                note.flag = flag
+            }
+            note.time = NSDate()
+            note.locationID = location?.id
+            note.locationAmbiguous = false
+            note.radius = 0
+            return note
+        }
+        
+        return nil
+    }
+    
+    
 }
