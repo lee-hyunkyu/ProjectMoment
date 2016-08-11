e99057d98c944c651c48caef194106c020683116
Hyunkyu Lee <hkl0902@gmail.com>
2016-08-11 15:15:08 -0700

    Adds class to be the view for Notebook custom cells

---
+++ ProjectMoment/NotebookTableCellView.swift
@@ -0,0 +1,21 @@
+//
+//  NotebookTableCellView.swift
+//  ProjectMoment
+//
+//  Created by Hyunkyu Lee on 8/11/16.
+//  Copyright © 2016 Hyunkyu Lee. All rights reserved.
+//
+
+import UIKit
+
+class NotebookTableCellView: UIView {
+
+    
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    
+
+}
